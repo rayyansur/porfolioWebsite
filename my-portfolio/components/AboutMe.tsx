79a68a424552be05c797091404441fb7a13c2105
rayyansurani <rayyan.surani@hotmail.com>
2026-01-29 15:03:36 -0500

    Updated about me to reflect a proper home welcome page.

--- my-portfolio/components/AboutMe.tsx
+++ my-portfolio/components/AboutMe.tsx
@@ -17,8 +17,11 @@ export default function AboutMe() {
 
             <p className="max-w-3xl mx-auto flex flex-col items-center text-lg opacity-80">
                 I am a Junior Computer Science major at Georgia Tech<br/>
-                This website works is my portfolio<br/>
-
+                This website is my portfolio to showcase all my academic and personal projects.<br/>
+                Most of my projects have either full-stack or back-end development done by me.<br/>
+                I am most interested in Computer Vision and a lot of my projects are centered around it. <br/>
+                I am proficient with frameworks like Django and React and programming languages like Java, Python, C++. <br/>
+                You can find out more about my projects and my contact details from the menu. <br/>
             </p>
 
         </section>
